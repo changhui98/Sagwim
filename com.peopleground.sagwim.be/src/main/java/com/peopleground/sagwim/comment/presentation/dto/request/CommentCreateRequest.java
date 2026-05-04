71ef9a30f57ffdd898713d8a10365b695080eb0a
changhui98 <net_0102@naver.com>
2026-05-05 06:46:16 +0900

    Feat : 댓글 이미지 URL 저장 지원 - Comment 엔티티 및 DTO에 imageUrl 필드 추가
    - Comment.java: imageUrl 컬럼 추가, of/ofReply 팩토리 메서드에 imageUrl 파라미터 추가
    - CommentCreateRequest.java: imageUrl 필드 추가
    - CommentResponse.java: imageUrl, authorProfileImageUrl, likedByMe 필드 추가
      비로그인 단건 응답용 from(Comment) 오버로드 메서드 추가
    - ImageTargetType.java: COMMENT 타입 추가

--- com.peopleground.sagwim.be/src/main/java/com/peopleground/sagwim/comment/presentation/dto/request/CommentCreateRequest.java
+++ com.peopleground.sagwim.be/src/main/java/com/peopleground/sagwim/comment/presentation/dto/request/CommentCreateRequest.java
@@ -6,7 +6,8 @@ import jakarta.validation.constraints.Size;
 public record CommentCreateRequest(
     @NotBlank(message = "댓글 내용을 입력해주세요.")
     @Size(max = 500, message = "댓글은 500자 이하로 입력해주세요.")
-    String body
+    String body,
+    String imageUrl
 ) {
 
 }
